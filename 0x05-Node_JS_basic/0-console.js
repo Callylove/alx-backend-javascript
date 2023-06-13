5ca1508c803d0a5c0e2bf9a71932e33a89e99353
Callylove <nwekechidimma88@gmail.com>
2023-06-13 09:27:04 +0100

    solved

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,5 @@
+// a function to write to stdout
+export function displayMessage(msg){
+	console.log(msg);
+};
+
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+// Importing the 'readline' module for reading user input
+const readline = require('readline');
+
+// Creating a readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Welcome to Holberton School, what is your name?");
+
+// waiting for input
+rl.on('line', (input)=>{console.log(`Your name is:`, input)});
+
+// listen for closing
+rl.on('close', ()=>{console.log('This important software is now closing')});
